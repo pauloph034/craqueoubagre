132a77e1dc79c79a76445c654900917420055e13
Paulo Rocha <pauloph034@gmail.com>
2026-07-28 09:26:20 -0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: process.env.NEXT_DIST_DIR || ".next",
   reactStrictMode: true,
   typedRoutes: true,
+  poweredByHeader: false,
   allowedDevOrigins: ["127.0.0.1"],
   images: {
     remotePatterns: [
